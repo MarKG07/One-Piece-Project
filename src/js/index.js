0749bde265890f3b743e161bbe35585936e9df8a
Marcus Silva <32456627+MarKG07@users.noreply.github.com>
2023-10-09 17:01:00 -0300

    javascript final

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,23 @@
 const botoes = document.querySelectorAll('.botao')
+const personagens = document.querySelectorAll('.personagem')
 
-botoes.forEach(botao => { //percorre todos os botoes
+botoes.forEach((botao, indice) => { //percorre todos os botoes
     botao.addEventListener('click', () => { //coloca escuta em cada botao
 
-        const botaoSelecionado = document.querySelector(".botao.selecionado")//selecionar o botao q tem essas classes
-        botaoSelecionado.classList.remove("selecionado")//remove o botao que tiver essa classe
+        desselecionarBotao()
+        desselecionarPersonagem()
 
         botao.classList.add("selecionado") //adiciona classe no botao clicado
-
-        const personagens = document.querySelectorAll('.personagem')
-        personagens.forEach(personagem => {
-            console.log(personagem)
-        })
+        personagens[indice].classList.add("selecionado")//usa o indice do foreach do botao para escolher qual div precisa mudar
     })
 })
 
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado") //selecionar o botao q tem essas classes
+    botaoSelecionado.classList.remove("selecionado")//remove o botao que tiver essa classe
+}
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado")
+    personagemSelecionado.classList.remove("selecionado")
+}
